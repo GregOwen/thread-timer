b3463472f01029b485c07c8e58ac13da5b08bfc0
GregOwen <greowen@gmail.com>
2020-10-07 14:57:50 -0400

    new rustfmt.toml

--- src/lib.rs
+++ src/lib.rs
@@ -2,29 +2,29 @@
 
 #![deny(missing_docs)]
 
-use std::sync::{Arc, Condvar, Mutex, TryLockError};
 use std::sync::mpsc::{self, Sender};
+use std::sync::{Arc, Condvar, Mutex, TryLockError};
 use std::thread;
 use std::time::Duration;
 
 /// Errors that may be thrown by ThreadTimer::start()
 #[derive(Debug, Eq, PartialEq)]
 pub enum TimerStartError {
-    /// The timer is already waiting to execute some other thunk
-    AlreadyWaiting,
+  /// The timer is already waiting to execute some other thunk
+  AlreadyWaiting,
 }
 
 /// Errors that may be thrown by ThreadTimer::cancel()
 #[derive(Debug, Eq, PartialEq)]
 pub enum TimerCancelError {
-    /// The timer is not currently waiting, so there is nothing to cancel
-    NotWaiting,
+  /// The timer is not currently waiting, so there is nothing to cancel
+  NotWaiting,
 }
 
 /// Message sent to tell the timer thread to start waiting
 struct StartWaitMessage {
-    dur: Duration,
-    f: Box<dyn FnOnce() + Send + 'static>,
+  dur: Duration,
+  f: Box<dyn FnOnce() + Send + 'static>,
 }
 
 /// A simple, cancelable timer that can run a thunk after waiting for an
@@ -73,166 +73,170 @@ struct StartWaitMessage {
 /// ```
 #[derive(Clone)]
 pub struct ThreadTimer {
-    // Allow only one operation at a time so that we don't need to worry about interleaving
-    op_lock: Arc<Mutex<()>>,
-    // Record whether or not the timer is currently waiting
-    is_waiting: Arc<Mutex<bool>>,
-    // Used to wait for a cancelation signal and avoid spurious wakeups while waiting
-    is_canceled: Arc<(Mutex<bool>, Condvar)>,
-    // Used to tell the timer thread to start waiting
-    sender: Sender<StartWaitMessage>,
+  // Allow only one operation at a time so that we don't need to worry about interleaving
+  op_lock: Arc<Mutex<()>>,
+  // Record whether or not the timer is currently waiting
+  is_waiting: Arc<Mutex<bool>>,
+  // Used to wait for a cancelation signal and avoid spurious wakeups while waiting
+  is_canceled: Arc<(Mutex<bool>, Condvar)>,
+  // Used to tell the timer thread to start waiting
+  sender: Sender<StartWaitMessage>,
 }
 
 impl ThreadTimer {
-    /// Creates and returns a new ThreadTimer. Spawns a new thread to do the
-    /// waiting (the "wait thread").
-    ///
-    /// ```
-    /// use thread_timer::ThreadTimer;
-    /// let timer = ThreadTimer::new();
-    /// ```
-    pub fn new() -> Self {
-	let (sender, receiver) = mpsc::channel::<StartWaitMessage>();
-	let is_waiting = Arc::new(Mutex::new(false));
-	let thread_is_waiting = is_waiting.clone();
-	let is_canceled = Arc::new((Mutex::new(false), Condvar::new()));
-	let thread_is_canceled = is_canceled.clone();
+  /// Creates and returns a new ThreadTimer. Spawns a new thread to do the
+  /// waiting (the "wait thread").
+  ///
+  /// ```
+  /// use thread_timer::ThreadTimer;
+  /// let timer = ThreadTimer::new();
+  /// ```
+  pub fn new() -> Self {
+    let (sender, receiver) = mpsc::channel::<StartWaitMessage>();
+    let is_waiting = Arc::new(Mutex::new(false));
+    let thread_is_waiting = is_waiting.clone();
+    let is_canceled = Arc::new((Mutex::new(false), Condvar::new()));
+    let thread_is_canceled = is_canceled.clone();
 
-	thread::spawn(move || {
-	    // Loop waiting for a new message from the client thread(s).  If all
-	    // senders have disconnected, we will never receive a new message so
-	    // we should break out of the loop.
-	    while let Ok(msg) = receiver.recv() {
-		let (cancel_lock, cancel_condvar) = &*thread_is_canceled;
-		let (mut cancel_guard, cancel_res) = cancel_condvar.wait_timeout_while(
-		    cancel_lock.lock().unwrap(),
-		    msg.dur,
-		    |&mut is_canceled| !is_canceled,
-		).unwrap();
-		if cancel_res.timed_out() {
-		    // Only run the thunk if the wait completed (i.e. it
-		    // was not canceled)
-		    (msg.f)();
-		}
-		// Always clear the cancel guard (even if the wait
-		// completed and we executed the thunk)
-		*cancel_guard = false;
-		*thread_is_waiting.lock().unwrap() = false;
-	    };
-	});
+    thread::spawn(move || {
+      // Loop waiting for a new message from the client thread(s).  If all
+      // senders have disconnected, we will never receive a new message so
+      // we should break out of the loop.
+      while let Ok(msg) = receiver.recv() {
+        let (cancel_lock, cancel_condvar) = &*thread_is_canceled;
+        let (mut cancel_guard, cancel_res) = cancel_condvar
+          .wait_timeout_while(
+            cancel_lock.lock().unwrap(),
+            msg.dur,
+            |&mut is_canceled| !is_canceled,
+          )
+          .unwrap();
+        if cancel_res.timed_out() {
+          // Only run the thunk if the wait completed (i.e. it
+          // was not canceled)
+          (msg.f)();
+        }
+        // Always clear the cancel guard (even if the wait
+        // completed and we executed the thunk)
+        *cancel_guard = false;
+        *thread_is_waiting.lock().unwrap() = false;
+      }
+    });
 
-	ThreadTimer {
-	    op_lock: Arc::new(Mutex::new(())),
-	    is_waiting,
-	    is_canceled,
-	    sender,
-	}
+    ThreadTimer {
+      op_lock: Arc::new(Mutex::new(())),
+      is_waiting,
+      is_canceled,
+      sender,
+    }
+  }
+
+  /// Start waiting. Wait for `dur` to elapse then execute `f`. Will not
+  /// execute `f` if the timer is canceled before `dur` elapses.
+  /// Returns [TimerStartError](enum.TimerStartError.html)::AlreadyWaiting if
+  /// the timer is already waiting to execute a thunk.
+  /// ```
+  /// use std::sync::mpsc::{self, TryRecvError};
+  /// use std::thread;
+  /// use std::time::Duration;
+  /// use thread_timer::ThreadTimer;
+  ///
+  /// let (sender, receiver) = mpsc::channel::<bool>();
+  /// let timer = ThreadTimer::new();
+  ///
+  /// timer.start(Duration::from_millis(50), move || { sender.send(true).unwrap() }).unwrap();
+  /// assert_eq!(
+  ///   receiver.try_recv(),
+  ///   Err(TryRecvError::Empty),
+  ///   "Received response before wait elapsed!",
+  /// );
+  ///
+  /// thread::sleep(Duration::from_millis(60));
+  /// assert_eq!(
+  ///   receiver.try_recv(),
+  ///   Ok(true),
+  ///   "Did not receive response after wait elapsed!",
+  /// );
+  /// ```
+  pub fn start<F>(&self, dur: Duration, f: F) -> Result<(), TimerStartError>
+  where
+    F: FnOnce() + Send + 'static,
+  {
+    let _guard = self.op_lock.lock().unwrap();
+    let mut is_waiting = self.is_waiting.lock().unwrap();
+    if *is_waiting {
+      return Err(TimerStartError::AlreadyWaiting);
+    }
+    *is_waiting = true;
+    let msg = StartWaitMessage {
+      dur,
+      f: Box::new(f),
+    };
+    self.sender.send(msg).unwrap();
+    Ok(())
+  }
+
+  /// Cancel the current timer (the thunk will not be executed and the timer
+  /// will be able to start waiting to execute another thunk).
+  /// Returns [TimerCancelError](enum.TimerCancelError.html)::NotWaiting if
+  /// the timer is not currently waiting.
+  /// ```
+  /// use std::sync::mpsc::{self, TryRecvError};
+  /// use std::thread;
+  /// use std::time::Duration;
+  /// use thread_timer::ThreadTimer;
+  ///
+  /// let (sender, receiver) = mpsc::channel::<bool>();
+  /// let timer = ThreadTimer::new();
+  ///
+  /// timer.start(Duration::from_millis(50), move || { sender.send(true).unwrap() }).unwrap();
+  ///
+  /// // Make sure the wait has actually started before we cancel
+  /// thread::sleep(Duration::from_millis(10));
+  /// timer.cancel().unwrap();
+  ///
+  /// thread::sleep(Duration::from_millis(60));
+  /// assert_eq!(
+  ///   receiver.try_recv(),
+  ///   // When the wait is canceled, the thunk and its Sender will be dropped
+  ///   Err(TryRecvError::Disconnected),
+  ///   "Received response from canceled wait!",
+  /// );
+  /// ```
+  pub fn cancel(&self) -> Result<(), TimerCancelError> {
+    let _guard = self.op_lock.lock().unwrap();
+    let is_waiting = self.is_waiting.lock().unwrap();
+    if !*is_waiting {
+      return Err(TimerCancelError::NotWaiting);
     }
 
-    /// Start waiting. Wait for `dur` to elapse then execute `f`. Will not
-    /// execute `f` if the timer is canceled before `dur` elapses.
-    /// Returns [TimerStartError](enum.TimerStartError.html)::AlreadyWaiting if
-    /// the timer is already waiting to execute a thunk.
-    /// ```
-    /// use std::sync::mpsc::{self, TryRecvError};
-    /// use std::thread;
-    /// use std::time::Duration;
-    /// use thread_timer::ThreadTimer;
-    ///
-    /// let (sender, receiver) = mpsc::channel::<bool>();
-    /// let timer = ThreadTimer::new();
-    ///
-    /// timer.start(Duration::from_millis(50), move || { sender.send(true).unwrap() }).unwrap();
-    /// assert_eq!(
-    ///   receiver.try_recv(),
-    ///   Err(TryRecvError::Empty),
-    ///   "Received response before wait elapsed!",
-    /// );
-    ///
-    /// thread::sleep(Duration::from_millis(60));
-    /// assert_eq!(
-    ///   receiver.try_recv(),
-    ///   Ok(true),
-    ///   "Did not receive response after wait elapsed!",
-    /// );
-    /// ```
-    pub fn start<F>(&self, dur: Duration, f: F) -> Result<(), TimerStartError>
-    where F: FnOnce() + Send + 'static {
-	let _guard = self.op_lock.lock().unwrap();
-	let mut is_waiting = self.is_waiting.lock().unwrap();
-	if *is_waiting {
-	    return Err(TimerStartError::AlreadyWaiting);
-	}
-	*is_waiting = true;
-	let msg = StartWaitMessage {
-	    dur,
-	    f: Box::new(f),
-	};
-	self.sender.send(msg).unwrap();
-	Ok(())
+    let (cancel_lock, cancel_condvar) = &*self.is_canceled;
+
+    // This must be try_lock() not lock() in order to avoid a deadlock with
+    // the wait thread. At this point the client thread holds the wait
+    // lock. If the wait thread holds the cancel lock (if it has finished
+    // waiting and is running the task), then we will not be able to get the
+    // cancel lock here and the wait thread will not be able to get the wait
+    // lock to indicate that it has finished waiting.
+    match cancel_lock.try_lock() {
+      // We were able to acquire the cancel lock, so cancel the wait
+      Ok(mut cancel_guard) => {
+        *cancel_guard = true;
+        cancel_condvar.notify_one();
+        // We do not clear the cancel or waiting flags here since those
+        // are cleared in the wait thread
+        Ok(())
+      }
+      // The wait thread holds the cancel lock, so return an error
+      // indicating that we were unable to cancel
+      Err(TryLockError::WouldBlock) => Err(TimerCancelError::NotWaiting),
+      Err(TryLockError::Poisoned(_)) => panic!("Cancel lock was poisoned"),
     }
-
-    /// Cancel the current timer (the thunk will not be executed and the timer
-    /// will be able to start waiting to execute another thunk).
-    /// Returns [TimerCancelError](enum.TimerCancelError.html)::NotWaiting if
-    /// the timer is not currently waiting.
-    /// ```
-    /// use std::sync::mpsc::{self, TryRecvError};
-    /// use std::thread;
-    /// use std::time::Duration;
-    /// use thread_timer::ThreadTimer;
-    ///
-    /// let (sender, receiver) = mpsc::channel::<bool>();
-    /// let timer = ThreadTimer::new();
-    ///
-    /// timer.start(Duration::from_millis(50), move || { sender.send(true).unwrap() }).unwrap();
-    ///
-    /// // Make sure the wait has actually started before we cancel
-    /// thread::sleep(Duration::from_millis(10));
-    /// timer.cancel().unwrap();
-    ///
-    /// thread::sleep(Duration::from_millis(60));
-    /// assert_eq!(
-    ///   receiver.try_recv(),
-    ///   // When the wait is canceled, the thunk and its Sender will be dropped
-    ///   Err(TryRecvError::Disconnected),
-    ///   "Received response from canceled wait!",
-    /// );
-    /// ```
-    pub fn cancel(&self) -> Result<(), TimerCancelError> {
-	let _guard = self.op_lock.lock().unwrap();
-	let is_waiting = self.is_waiting.lock().unwrap();
-	if !*is_waiting {
-	    return Err(TimerCancelError::NotWaiting);
-	}
-
-	let (cancel_lock, cancel_condvar) = &*self.is_canceled;
-
-	// This must be try_lock() not lock() in order to avoid a deadlock with
-	// the wait thread. At this point the client thread holds the wait
-	// lock. If the wait thread holds the cancel lock (if it has finished
-	// waiting and is running the task), then we will not be able to get the
-	// cancel lock here and the wait thread will not be able to get the wait
-	// lock to indicate that it has finished waiting.
-	match cancel_lock.try_lock() {
-	    // We were able to acquire the cancel lock, so cancel the wait
-	    Ok(mut cancel_guard) => {
-		*cancel_guard = true;
-		cancel_condvar.notify_one();
-		// We do not clear the cancel or waiting flags here since those
-		// are cleared in the wait thread
-		Ok(())
-	    },
-	    // The wait thread holds the cancel lock, so return an error
-	    // indicating that we were unable to cancel
-	    Err(TryLockError::WouldBlock) => Err(TimerCancelError::NotWaiting),
-	    Err(TryLockError::Poisoned(_)) => panic!("Cancel lock was poisoned"),
-	}
-    }
+  }
 }
 
 impl Default for ThreadTimer {
-    fn default() -> Self {
-	Self::new()
-    }
+  fn default() -> Self {
+    Self::new()
+  }
 }
--- tests/thread_timer.rs
+++ tests/thread_timer.rs
@@ -5,82 +5,82 @@ use std::time::Duration;
 use thread_timer::*;
 
 fn get_test_thunk(sender: &Sender<bool>) -> impl FnOnce() + Send + 'static {
-    let sender_clone = sender.clone();
-    move || {
-	sender_clone.send(true).unwrap();
-    }
+  let sender_clone = sender.clone();
+  move || {
+    sender_clone.send(true).unwrap();
+  }
 }
 
 #[test]
 fn executes_thunk_after_wait() {
-    let timer = ThreadTimer::new();
-    let (sender, receiver) = mpsc::channel();
-    let f = get_test_thunk(&sender);
-    assert_eq!(timer.start(Duration::from_millis(50), f), Ok(()));
-    assert_eq!(receiver.try_recv(), Err(TryRecvError::Empty));
-    thread::sleep(Duration::from_millis(60));
-    assert_eq!(receiver.try_recv(), Ok(true));
+  let timer = ThreadTimer::new();
+  let (sender, receiver) = mpsc::channel();
+  let f = get_test_thunk(&sender);
+  assert_eq!(timer.start(Duration::from_millis(50), f), Ok(()));
+  assert_eq!(receiver.try_recv(), Err(TryRecvError::Empty));
+  thread::sleep(Duration::from_millis(60));
+  assert_eq!(receiver.try_recv(), Ok(true));
 }
 
 #[test]
 fn can_be_reused() {
-    let timer = ThreadTimer::new();
-    let (sender, receiver) = mpsc::channel();
-    let f1 = get_test_thunk(&sender);
-    assert_eq!(timer.start(Duration::from_millis(50), f1), Ok(()));
-    thread::sleep(Duration::from_millis(60));
-    assert_eq!(receiver.try_recv(), Ok(true));
+  let timer = ThreadTimer::new();
+  let (sender, receiver) = mpsc::channel();
+  let f1 = get_test_thunk(&sender);
+  assert_eq!(timer.start(Duration::from_millis(50), f1), Ok(()));
+  thread::sleep(Duration::from_millis(60));
+  assert_eq!(receiver.try_recv(), Ok(true));
 
-    let f2 = get_test_thunk(&sender);
-    assert_eq!(timer.start(Duration::from_millis(50), f2), Ok(()));
-    thread::sleep(Duration::from_millis(60));
-    assert_eq!(receiver.try_recv(), Ok(true));
+  let f2 = get_test_thunk(&sender);
+  assert_eq!(timer.start(Duration::from_millis(50), f2), Ok(()));
+  thread::sleep(Duration::from_millis(60));
+  assert_eq!(receiver.try_recv(), Ok(true));
 }
 
 #[test]
 fn cannot_cancel_if_not_waiting() {
-    let timer = ThreadTimer::new();
-    assert_eq!(timer.cancel(), Err(TimerCancelError::NotWaiting));
+  let timer = ThreadTimer::new();
+  assert_eq!(timer.cancel(), Err(TimerCancelError::NotWaiting));
 }
 
 #[test]
 fn thunk_does_not_run_if_canceled() {
-    let timer = ThreadTimer::new();
-    let (sender, receiver) = mpsc::channel();
-    let f = get_test_thunk(&sender);
-    assert_eq!(timer.start(Duration::from_millis(50), f), Ok(()));
-    thread::sleep(Duration::from_millis(10));
-    assert_eq!(timer.cancel(), Ok(()));
-    thread::sleep(Duration::from_millis(75));
-    assert_eq!(receiver.try_recv(), Err(TryRecvError::Empty));
+  let timer = ThreadTimer::new();
+  let (sender, receiver) = mpsc::channel();
+  let f = get_test_thunk(&sender);
+  assert_eq!(timer.start(Duration::from_millis(50), f), Ok(()));
+  thread::sleep(Duration::from_millis(10));
+  assert_eq!(timer.cancel(), Ok(()));
+  thread::sleep(Duration::from_millis(75));
+  assert_eq!(receiver.try_recv(), Err(TryRecvError::Empty));
 }
 
 #[test]
 fn does_not_deadlock_when_canceling_long_task() {
-    // TODO(greg): put a timeout on this test so that we get a failure rather than a hang
-    let timer = ThreadTimer::new();
-    let (sender, _) = mpsc::channel();
-    let f = move || {
-	thread::sleep(Duration::from_secs(10));
-	sender.send(true).unwrap();
-    };
-    assert_eq!(timer.start(Duration::from_millis(10), f), Ok(()));
-    thread::sleep(Duration::from_millis(20));
-    assert_eq!(timer.cancel(), Err(TimerCancelError::NotWaiting));
+  // TODO(greg): put a timeout on this test so that we get a failure rather than a hang
+  let timer = ThreadTimer::new();
+  let (sender, _) = mpsc::channel();
+  let f = move || {
+    thread::sleep(Duration::from_secs(10));
+    sender.send(true).unwrap();
+  };
+  assert_eq!(timer.start(Duration::from_millis(10), f), Ok(()));
+  thread::sleep(Duration::from_millis(20));
+  assert_eq!(timer.cancel(), Err(TimerCancelError::NotWaiting));
 }
 
 #[test]
 fn can_start_cancel_start() {
-    let timer = ThreadTimer::new();
-    let (sender, receiver) = mpsc::channel();
-    let f1 = get_test_thunk(&sender);
-    assert_eq!(timer.start(Duration::from_millis(50), f1), Ok(()));
-    thread::sleep(Duration::from_millis(10));
-    assert_eq!(timer.cancel(), Ok(()));
-    thread::sleep(Duration::from_millis(75));
-    assert_eq!(receiver.try_recv(), Err(TryRecvError::Empty));
-    let f2 = get_test_thunk(&sender);
-    assert_eq!(timer.start(Duration::from_millis(50), f2), Ok(()));
-    thread::sleep(Duration::from_millis(60));
-    assert_eq!(receiver.try_recv(), Ok(true))
+  let timer = ThreadTimer::new();
+  let (sender, receiver) = mpsc::channel();
+  let f1 = get_test_thunk(&sender);
+  assert_eq!(timer.start(Duration::from_millis(50), f1), Ok(()));
+  thread::sleep(Duration::from_millis(10));
+  assert_eq!(timer.cancel(), Ok(()));
+  thread::sleep(Duration::from_millis(75));
+  assert_eq!(receiver.try_recv(), Err(TryRecvError::Empty));
+  let f2 = get_test_thunk(&sender);
+  assert_eq!(timer.start(Duration::from_millis(50), f2), Ok(()));
+  thread::sleep(Duration::from_millis(60));
+  assert_eq!(receiver.try_recv(), Ok(true))
 }
